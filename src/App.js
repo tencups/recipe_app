0ba78f9c332b6af2f1b55a7f19cae6f840568643
tencups <26286782+tencups@users.noreply.github.com>
2019-07-30 12:24:04 -0400

    yoit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
+const App = () => {
+  
+  const APP_ID = '2394fb79';
+  const APP_KEY = '2c632db3ecfe13444cb0d794fdcf58bb';
+
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  
+  const [counter, setCounter] = useState(0);
+
+
+  useEffect(()=> {
+    console.log('effect has been run');
+  }, []);
+ 
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <form className="search-form">
+      <input className ="search-bar" type="text"/>
+      <button className ="search-button" type="submit">Search</button>
+    </form>
+    <h1 onClick ={()=> setCounter(counter+1)}>{counter}</h1>
     </div>
   );
 }
